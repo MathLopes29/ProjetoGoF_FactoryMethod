73d2ffe4c6b3d79816685d2658d68c3d72641561
Matheus Lopes <98532951+MathLopes29@users.noreply.github.com>
2023-03-15 10:42:24 -0300

    Update Estudante.java

--- src/interfaceFatec/Estudante.java
+++ src/interfaceFatec/Estudante.java
@@ -4,22 +4,30 @@ import java.util.Scanner;
 
 public class Estudante implements Fatec{
 
+	String Nome;
+	int RA;
+	
+	public Estudante(String Nome, int RA) {
+		this.Nome = Nome;
+		this.RA = RA;
+	}
+	
 	@Override
-	public String DigiteNome(String Nome) {
+	public String DigiteNome() {
 		try(Scanner s = new Scanner(System.in)){
 			System.out.print("Digite seu Nome: ");
 			Nome = s.nextLine();
 			return Nome;
 		}
 	}
-
+	
 	@Override
-	public int DigiteRA(int RA) {
-		return RA;
+	public int DigiteRA() {
+		return RA;	
 	}
 	
 	public String ExibeEstudante(String Msg) {
 		return Msg;
 	}
-	
+
 }
